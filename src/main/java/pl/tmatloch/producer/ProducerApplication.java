097bddce4228a249a61037cade8952ef8270efd5
Tomasz Matloch (tomamat015) <tomamat015@student.polsl.pl>
2019-10-07 20:24:30 +0200

    fix import

--- src/main/java/pl/tmatloch/producer/ProducerApplication.java
+++ src/main/java/pl/tmatloch/producer/ProducerApplication.java
@@ -2,7 +2,6 @@ package pl.tmatloch.producer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 //@EnableDiscoveryClient
